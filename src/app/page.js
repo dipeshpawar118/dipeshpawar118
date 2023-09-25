be7071ed4a5ecd218937da3096186b858ad05d44
Dipesh  Pawar <dipesh.p@renewhr.com>
2023-09-26 00:24:46 +0530

    quick fix

--- src/app/page.js
+++ src/app/page.js
@@ -20,8 +20,8 @@ export default function Home() {
                 <Line />
                <PopularCategories />
                <Line />
-                <PopularSubCatogires />
-                <Line />
+                {/* <PopularSubCatogires />
+                <Line /> */}
                <PopularProducts/>
                <Line />
                 {/* <ShopByIndustries />
